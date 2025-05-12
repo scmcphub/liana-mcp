537b693fafa50b950dc3ae7c49ac2a35953e4818
shuang <shenghui.huang@unito.it>
2025-05-12 10:19:52 +0000

    update AdataState and import

--- src/liana_mcp/server/__init__.py
+++ src/liana_mcp/server/__init__.py
@@ -12,18 +12,24 @@ from.pl import pl_mcp
 
 class AdataState:
     def __init__(self):
-        self.adata_dic = {}
+        self.adata_dic = {"exp": {}}
         self.active_id = None
+        self.metadata = {}
 
-    def get_adata(self, sampleid=None):
-        if self.active_id is None:
-            return None
-        sampleid = sampleid or self.active_id
-        return self.adata_dic[sampleid]
+    def get_adata(self, sampleid=None, dtype="exp"):
+        try:
+            if self.active_id is None:
+                return None
+            sampleid = sampleid or self.active_id
+            return self.adata_dic[dtype][sampleid]
+        except KeyError as e:
+            raise KeyError(f"Key {e} not found in adata_dic")
+        except Exception as e:
+            raise Exception(f"Error: {e}")
     
-    def set_adata(self, adata, sampleid=None):
+    def set_adata(self, adata, sampleid=None, sdtype="exp"):
         sampleid = sampleid or self.active_id
-        self.adata_dic[sampleid] = adata
+        self.adata_dic[sdtype][sampleid] = adata
 
 
 ads = AdataState()
@@ -35,7 +41,13 @@ async def adata_lifespan(server: FastMCP) -> AsyncIterator[Any]:
 liana_mcp = FastMCP("Liana-MCP-Server", lifespan=adata_lifespan)
 
 
-async def setup():
-    await liana_mcp.import_server("io", io_mcp)
-    await liana_mcp.import_server("ccc", ccc_mcp)
-    await liana_mcp.import_server("pl", pl_mcp) 
+async def setup(modules=None):
+    mcp_dic = {
+        "io": io_mcp, 
+        "ccc": ccc_mcp, 
+        "pl": pl_mcp, 
+        }
+    if modules is None or modules == "all":
+        modules = ["io", "ccc", "pl"]
+    for module in modules:
+        await liana_mcp.import_server(module, mcp_dic[module])
